d239cfbd222e7501505e57220741c6d399d1f72c
sd-omkar <sd.omkar@gmail.com>
2013-09-30 22:28:21 -0500

    lottery try1

--- p2/xv6/kernel/proc.c
+++ p2/xv6/kernel/proc.c
@@ -265,19 +265,40 @@ scheduler(void)
   for(;;){
     // Enable interrupts on this processor.
     sti();
+    int tickets = 0;
 
-    // Loop over process table looking for process to run.
+    // Count the number of tickets
     acquire(&ptable.lock);
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
       if(p->state != RUNNABLE)
         continue;
+      tickets += p->tickets;
+    }
 
+    if (tickets > 0) {
+      int winner = random();
+      if (winner < 0)
+        winner *= -1;
+      tickets = winner % tickets;
+    }
+
+    // Loop over process table looking for process to run.
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      if(p->state != RUNNABLE)
+        continue;
+      
+      tickets -= p->tickets;
+      if (tickets > 0)
+        continue;
       // Switch to chosen process.  It is the process's job
       // to release ptable.lock and then reacquire it
       // before jumping back to us.
       proc = p;
       switchuvm(p);
       p->state = RUNNING;
+      p->tickets -= 1;
+      if (p->tickets == 0)
+        settickets(5);
       swtch(&cpu->scheduler, proc->context);
       switchkvm();
 
@@ -479,4 +500,20 @@ int getpinfo(void) {
   }
   release(&ptable.lock);
   return 0;
+}
+
+// Credits http://www.iro.umontreal.ca/~simardr/rng/lfsr113.c
+int random(void)
+{
+  static int z1 = 12345, z2 = 12345, z3 = 12345, z4 = 12345;
+  int b;
+  b  = ((z1 << 6) ^ z1) >> 13;
+  z1 = ((z1 & 4294967294U) << 18) ^ b;
+  b  = ((z2 << 2) ^ z2) >> 27; 
+  z2 = ((z2 & 4294967288U) << 2) ^ b;
+  b  = ((z3 << 13) ^ z3) >> 21;
+  z3 = ((z3 & 4294967280U) << 7) ^ b;
+  b  = ((z4 << 3) ^ z4) >> 12;
+  z4 = ((z4 & 4294967168U) << 13) ^ b;
+  return (z1 ^ z2 ^ z3 ^ z4);
 }
